c6e1824c28d3690ceb9677197c8b3be09c194bbc
Ivan Burenko <Roldex@list.ru>
2023-05-05 08:13:41 +0300

    linking code and UI in main.py

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ from PyQt6 import QtCore, QtWidgets
 import sys
 
 
-class Generator(QtWidgets.QMainWindow):
+class PassGenerator(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Generator, self).__init__()
+        super(PassGenerator, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.init_UI()
 
     def init_UI(self):
-        pass
+        self.ui.pushButton_generate.clicked.connect(self.Generate)
+
+    def Generate(self):
+        password_length = self.ui.spinBox_passlength.value()
+
+        if self.ui.radioButton_md5.isChecked():
+            password = f'MD5 {password_length}'
+        else:
+            password = f'SHA256 {password_length}'
+
+        self.ui.textEdit_password.setText(password)
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    application = Generator()
+    application = PassGenerator()
     application.show()
 
     sys.exit(app.exec())
